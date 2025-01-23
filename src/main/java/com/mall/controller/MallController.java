53e2f3f93f85b14371b81df46c3ab69f35170e11
NakJoo <169232628+NJ0428@users.noreply.github.com>
2025-01-23 22:00:12 +0900

    몰추가

---
+++ src/main/java/com/mall/controller/MallController.java
@@ -0,0 +1,21 @@
+package com.mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@Log4j2
+@RequiredArgsConstructor//클래스내부의 필드중 private final 로 선언된 inteface 의 하위 객체를 자동 주입시키는 어노테이션임.
+@RequestMapping("/mall")//localhost/ditto(context)/list(path)
+public class MallController {
+    @GetMapping(value = {"/main"})
+    public String main(){
+        return "/mall/mainMall";
+    }  
+}
+
+
